d4ada95636dd956a4ad63cefa13125752fe9b42e
AliceRolanThomas <2023aa05453@wilp.bits-pilani.ac.in>
2025-01-23 02:03:11 +0530

    Liniting Fix

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -4,6 +4,6 @@ from src.predict import predict_category
 def test_predict():
     text = "A significant political event happened today"
     prediction = predict_category(text)
-    valid_categories = ['alt.atheism', 'comp.graphics', 
+    valid_categories = ['alt.atheism', 'comp.graphics',
                         'sci.med', 'rec.sport.hockey', 'talk.politics.misc']
     assert prediction in valid_categories
